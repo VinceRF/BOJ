295a3a9c15366e42ac27a2e243be0aa8da8efd4c
DESKTOP-L0R7VNG\Foryoucom <a>
2025-09-10 04:05:50 +0900

    B5_Day1

---
+++ src/B5_Day1/BOJ10718.java
@@ -0,0 +1,11 @@
+package B5_Day1;
+
+public class BOJ10718 {
+    public static void main(String[] args) {
+        System.out.println("강한친구 대한육군");
+        System.out.println("강한친구 대한육군");
+//        한 줄 출력 ln 제거
+//        System.out.print("강한친구 대한육군");
+//        System.out.print("강한친구 대한육군");
+    }
+}
