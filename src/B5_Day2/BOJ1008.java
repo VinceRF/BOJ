5eb3dbdb90ac6862adef8bf979d23769ac6260f0
VinceRF <zhsk30@naver.com>
2025-09-11 13:50:16 +0900

    B5_Day2 생성

---
+++ src/B5_Day2/BOJ1008.java
@@ -0,0 +1,12 @@
+package B5_Day2;
+
+import java.util.Scanner;
+
+public class BOJ1008 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double A = sc.nextInt();
+        double B = sc.nextInt();
+        System.out.println(A/B);
+    }
+}
